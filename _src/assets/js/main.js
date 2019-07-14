39103da7211ea038ef5200716dcb481818c32391
Ines <inesp.pino@gmail.com>
2019-07-14 16:57:25 +0200

    fix: carrousel works

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('>> Ready :)');
+let slides = document.querySelectorAll('.carrousel .slide');
+let currentSlide = 0;
+
+function playSlideshow() {
+    setInterval(nextSlide,3000);
+}
+
+function nextSlide() {
+    goToSlide(currentSlide+1);
+}
+
+function goToSlide(n) {
+    slides[currentSlide].classList.remove('showing');
+    currentSlide = (n+slides.length)%slides.length;
+    slides[currentSlide].classList.add('showing');
+}
+
+playSlideshow();
