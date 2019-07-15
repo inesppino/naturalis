6ce5746568d7a66e7440ec16f1c99424e87e93f6
Inés Pedraza Pino <inesppino@mbp-de-ines.dhcp.mano.es.sopra>
2019-07-15 09:00:44 +0200

    feat: add detail views on map of the site

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,29 +4,49 @@ let slides = document.querySelectorAll('.carrousel .slide');
 let currentSlide = 0;
 
 function playSlideshow() {
-    setInterval(nextSlide,3000);
+  setInterval(nextSlide,3000);
 }
 
 function nextSlide() {
-    goToSlide(currentSlide+1);
+  goToSlide(currentSlide+1);
 }
 
 function goToSlide(n) {
-    slides[currentSlide].classList.remove('showing');
-    currentSlide = (n+slides.length)%slides.length;
-    slides[currentSlide].classList.add('showing');
+  slides[currentSlide].classList.remove('showing');
+  currentSlide = (n+slides.length)%slides.length;
+  slides[currentSlide].classList.add('showing');
 }
 
 playSlideshow();
 
 function addClassLit(){
-    const lithica = document.getElementById('li-cant');
-    lithica.classList.toggle('content-lithica');
-    lithica.classList.toggle('lithica');
+  const lithica = document.getElementById('li-cant');
+  lithica.classList.toggle('content-lithica');
+  lithica.classList.toggle('lithica');
 }
 
 function addClassLif(){
-    const life = document.getElementById('li-nat');
-    life.classList.toggle('content-natura');
-    life.classList.toggle('natura');
+  const life = document.getElementById('li-nat');
+  life.classList.toggle('content-natura');
+  life.classList.toggle('natura');
+}
+
+function showImg(place){
+  const selectedPlace = document.getElementById(place);
+  selectedPlace.classList.remove('mapa-detail');
+  if( place === 'map-viv-det'){
+    selectedPlace.classList.add('map-viv');
+  } else {
+    selectedPlace.classList.add('map-pal');
+  }
+}
+
+function hideImg(place){
+  const selectedPlace = document.getElementById(place);
+  selectedPlace.classList.add('mapa-detail');
+  if( place === 'map-viv-det'){
+    selectedPlace.classList.remove('map-viv');
+  } else {
+    selectedPlace.classList.remove('map-pal');
+  }
 }
